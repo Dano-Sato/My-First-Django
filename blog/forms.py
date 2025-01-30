3f3fff7d23c1f6bb264062648ceba7c475a71fe5
Dano Sato <ten0102@naver.com>
2025-01-30 23:06:51 +0900

    Form security advanced

--- blog/forms.py
+++ blog/forms.py
@@ -1,3 +1,4 @@
+import re,bleach
 from django import forms
 from .models import Post
 
@@ -5,3 +6,21 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+
+        # 제목에서 허용된 HTML 태그만 남기고 나머지 제거
+        allowed_tags = ['b', 'i', 'u', 'em', 'strong', 'a']
+        title = bleach.clean(title, tags=allowed_tags, strip=True)
+
+        return title
+
+    def clean_content(self):
+        content = self.cleaned_data.get('content')
+
+        # 내용에서 허용된 HTML 태그만 남기고 나머지 제거
+        allowed_tags = ['b', 'i', 'u', 'em', 'strong', 'a', 'p', 'ul', 'ol', 'li', 'blockquote']
+        content = bleach.clean(content, tags=allowed_tags, strip=True)
+
+        return content
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from .forms import PostForm
 from .models import Post
+from django.views.decorators.http import require_http_methods
 from django.utils import timezone
 
 def post_list(request):
     posts = Post.objects.order_by('-created_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+# @login_required  # 로그인한 사용자만 접근 가능 (아직 사용 안함)
+@require_http_methods(["GET", "POST"])  # GET과 POST 메소드만 허용
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
